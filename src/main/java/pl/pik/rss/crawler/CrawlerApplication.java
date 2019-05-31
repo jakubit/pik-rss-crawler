fb964167140993b82563aaf5c781af427a27a02a
jakub <pawlak011@gmail.com>
2019-05-31 22:49:17 +0200

    initial commit

---
+++ src/main/java/pl/pik/rss/crawler/CrawlerApplication.java
@@ -0,0 +1,13 @@
+package pl.pik.rss.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerApplication.class, args);
+    }
+
+}
