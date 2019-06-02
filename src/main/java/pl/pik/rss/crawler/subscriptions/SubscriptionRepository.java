55503c493dae6ecc3e1bceb7791797f0df2815c0
jakub <pawlak011@gmail.com>
2019-06-02 09:31:08 +0200

    Packages fixed

--- src/main/java/pl/pik/rss/crawler/subscriptions/SubscriptionRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.pik.rss.crawler.subscriptions;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface SubscriptionRepository extends CrudRepository<Subscription, String> {
-}
